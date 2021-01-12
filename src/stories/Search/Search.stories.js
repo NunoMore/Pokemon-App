dac73daf887dae53d8ca14732e499896305282dc
NunoMore <nunomoreira16@gmail.com>
2021-01-12 19:43:27 +0100

    fix search bar story

--- src/stories/Search/Search.stories.js
+++ src/stories/Search/Search.stories.js
@@ -11,7 +11,7 @@ export default {
 
 const Template = (args) => (
   <Provider store={store}>
-    <SearchBar {...args} />;
+    <SearchBar {...args} />
   </Provider>
 );
 export const SearchBarTest = Template.bind({});
